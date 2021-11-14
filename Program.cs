f00f60263a3c062442b906e0df549af6140dbd01
Legion59 <legion.59mk@gmail.com>
2021-11-14 21:14:27 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, new brunch!");
-Console.WriteLine("Conflict!");
+Console.WriteLine("This...!!! Is the first brunch.");
